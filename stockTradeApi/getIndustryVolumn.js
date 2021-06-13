08eb084753d4797fd19df70b41161abfc81ff1ae
Ken <ken.Yu@mile.cloud>
2021-06-14 02:26:24 +0800

    Add unselect weekend date

--- stockTradeApi/getIndustryVolumn.js
+++ stockTradeApi/getIndustryVolumn.js
@@ -1,14 +1,27 @@
 const axios = require("axios");
 const dayjs = require("dayjs");
-const stockServices = require("../services/stockServices");
 
-const today = dayjs().format("YYYYMMDD")
+const testDate = "20210611"
 
-const testDate = "20210528"
+// 排除六日股市沒有營業的日期，但未排除國定假日
+const getWeekDay = () => {
+    // Sunday
+    if (dayjs().weekday() === 0) {
+        return dayjs().weekday(-1).format("YYYYMMDD")
+    }
+    // Saterday
+    else if (dayjs().weekday() === 6) {
+        return dayjs().weekday(0).format("YYYYMMDD")
+    }
+    else {
+        return dayjs().format("YYYYMMDD")
+    }
+};
 
+const weekday = getWeekDay();
 
 const fetchIndustryVolumn = async () => {
-    const res = await axios.get(`https://www.twse.com.tw/exchangeReport/BFIAMU?response=json&date=${today}`);
+    const res = await axios.get(`https://www.twse.com.tw/exchangeReport/BFIAMU?response=json&date=${testDate}`);
 
     if (res.data.stat.includes("沒有")) {
         return
@@ -25,7 +38,7 @@ const formatData = (data) => {
             name: industry[0].trim(),
             tradingVolume: industry[1],
             percentage: industry[4],
-            dataSourceDate: today
+            dataSourceDate: testDate
         }
     })
 };
