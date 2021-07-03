d1b4d65ffb98f6059394b50c65677f67c34a2576
Ken <ken.Yu@mile.cloud>
2021-07-03 18:40:15 +0800

    Clean data

--- economicApi/getTwIndex.js
+++ economicApi/getTwIndex.js
@@ -4,6 +4,20 @@ const axios = require("axios");
 const fetchTwIndex = async () => {
     const res = await axios.get(`https://data.gov.tw/qc_download/dq_download_json.php?nid=13228&md5_url=7f7a62ecf322c57321d51cf863e2e035`);
 
+    const cleanData = (data) => {
+        const result = data.map(item => {
+            Object.keys(item).map(ele => {
+                item[ele] = item[ele].replace(/[a-zA-Z]/g, "").trim()
+            })
+
+            return item
+        })
+
+        // console.log(result)
+    }
+
+    cleanData(res.data)
+
     return res.data
 };
 
